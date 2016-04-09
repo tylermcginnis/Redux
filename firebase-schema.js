1e20c4583278da8e69c694ff470e4dceabeed160
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-04-08 19:50:56 -0600

    1-firebase-schema

---
+++ firebase-schema.js
@@ -0,0 +1,51 @@
+/users
+  uid
+    name
+    uid
+    avatar
+
+/notifications
+  uid
+    notificationId
+      type
+      author
+      authorAvatar
+      uid (of author)
+      duckId
+      timestamp
+
+/ducks
+  duckId
+    avatar
+    duckId
+    name
+    text
+    timestamp
+    uid (of duck author)
+
+/likeCount
+  duckId
+    0
+
+/usersDucks
+  uid
+    duckId
+      avatar
+      duckId
+      name
+      text
+      timestamp
+      uid (of duck author)
+
+/replies
+  duckId
+    replyId
+      name
+      comment
+      uid
+      timestamp
+      avatar
+
+/usersLikes
+  uid
+    duckId: true
