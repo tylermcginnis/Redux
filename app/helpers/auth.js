098ffc35f24a18e604a5d1fdc2e2db4079e9cf04
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2016-09-22 19:48:40 +0200

    Missing firebase import
    I believe there is a missing firebase import in the auth.js file.  The demo seems broken without it.

--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -1,4 +1,5 @@
 import { ref, firebaseAuth } from 'config/constants'
+import firebase from 'firebase'
 
 export default function auth () {
   return firebaseAuth().signInWithPopup(new firebase.auth.FacebookAuthProvider())
